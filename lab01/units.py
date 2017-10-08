b445f449a30efbe2e10c9ae9688bbfac135850e7
dianadyachun <32568783+dianadyachun@users.noreply.github.com>
2017-10-08 20:00:00 +0100

    Update units.py

--- lab01/units.py
+++ lab01/units.py
@@ -1,15 +1,19 @@
-num_kms = 1
-num_miles = 1.61*num_kms
-print 2*num_miles
+num_m = 800.
+print num_m
+num_miles = 1.61 * (num_m/1000)
+print num_miles
 
-num_kg = 1
-num_pounds = 6.35*num_kg
-print num_pounds
-num_stone = 14*num_pounds
-print num_stone
+num_sec = 130.
+num_min = num_sec / 60
+print num_min
 
-secs_in_min = 60
-num_meter = num_kms*1000
-print 800*num_meter
+num_min_per_mile = num_min / num_miles
+print num_min_per_mile
+
+minutes = int(num_min_per_mile)
+seconds = (num_min_per_mile*60) % 60
+miliseconds = (num_min_per_mile*3600) % 60
+
+print("%d:%02d:%02d" % (minutes, seconds, miliseconds))
 
 
